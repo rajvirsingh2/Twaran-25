e8b7551f2a1269c51008927d19d77356d91b05bb
rezaul-web <rmdrezaul75@gmail.com>
2025-03-04 15:45:56 +0530

    design done

--- app/src/main/java/com/example/twaran25/leaderboard/LeaderBoard.kt
+++ app/src/main/java/com/example/twaran25/leaderboard/LeaderBoard.kt
@@ -1,37 +1,78 @@
 package com.example.twaran25.leaderboard
 
 import android.os.Bundle
+import android.widget.PopupMenu
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import com.example.twaran25.R
+import com.example.twaran25.databinding.ActivityLeaderBoardBinding
 
 class LeaderBoard : AppCompatActivity() {
-    val players= listOf(
-        PlayerInfo(R.drawable.avatar_one,"John Doe",1,"IIIT GWALIOR"),
-        PlayerInfo(R.drawable.avatar_third,"John Doe",1,"IIIT GWALIOR"),
-        PlayerInfo(R.drawable.avatar_third,"John Doe",1,"IIIT GWALIOR"),
-        PlayerInfo(R.drawable.avatar_one,"John Doe",1,"IIIT GWALIOR"),
-        PlayerInfo(R.drawable.avatar_one,"John Doe",1,"IIIT GWALIOR"),
-        PlayerInfo(R.drawable.avatar_one,"John Doe",1,"IIIT GWALIOR")
+    private lateinit var binding: ActivityLeaderBoardBinding
+
+    val players = listOf(
+        PlayerInfo(R.drawable.avatar_one, "Rezaul", 1, "IIIT GWALIOR"),
+        PlayerInfo(R.drawable.avatar_third, "Owais", 2, "IIIT GWALIOR"),
+        PlayerInfo(R.drawable.avatar_third, "Rajat ", 3, "IIIT GWALIOR"),
+        PlayerInfo(R.drawable.avatar_one, "John Doe", 4, "IIIT GWALIOR"),
+        PlayerInfo(R.drawable.avatar_one, "John Doe", 5, "IIIT GWALIOR"),
+        PlayerInfo(R.drawable.avatar_one, "John Doe", 6, "IIIT GWALIOR")
     )
-   private lateinit var recyclerView:RecyclerView
+    val first = players.find {
+        it.place == 1
+    }
+    val second = players.find {
+        it.place == 2
+    }
+    val third = players.find {
+        it.place == 3
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_leader_board)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        // Initialize ViewBinding
+        binding = ActivityLeaderBoardBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Setup RecyclerView
+        binding.recyclerView.layoutManager = LinearLayoutManager(this)
+        binding.recyclerView.adapter = PlayerAdapter(players)
+
+        binding.playerFirstName.text= first?.name ?: ""
+        first?.let { binding.playerFirstImage.setImageResource(it.image) }
+
+        binding.playerSecondName.text= second?.name ?: ""
+        second?.let { binding.playerSecondImage.setImageResource(it.image) }
+
+        binding.playerThirdName.text= second?.name ?: ""
+        third?.let { binding.playerThirdImage.setImageResource(it.image) }
+
+        binding.filter.setOnClickListener { view->
+            val popupMenu=PopupMenu(this,view).apply {
+                menuInflater.inflate(R.menu.sports_menu,menu)
+                setOnMenuItemClickListener {
+                    when(it.itemId) {
+                        R.id.menu_cricket ->{
+                            binding.filterText.text="Cricket"
+                            true}
+                        R.id.menu_football->{
+                            binding.filterText.text="Football"
+                            true}
+                        R.id.menu_basketball->{
+                            binding.filterText.text="Basketball"
+                            true}
+                        R.id.menu_tennis->{true}
+                        else ->true
+                    }
+                }
+            }
+            popupMenu.show()
+
         }
-        recyclerView=findViewById(R.id.recycler_view)
-        recyclerView.layoutManager = LinearLayoutManager(this)
-        recyclerView.adapter=PlayerAdapter(players)
+
 
     }
-
 }
